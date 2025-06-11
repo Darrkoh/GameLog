f813e6134f53765f97e68fd7549139562645aed4
Darkoh <lukekirk05@gmail.com>
2025-06-11 21:14:13 +0100

    initial commit :)

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+/* 
+    Refined version of a program I made in 1st Year Uni.
+
+    I am making this one in Rust since Java is awful to work with lol
+
+    It will act as a game log for videogames, allowing users to:
+    - Add 
+    - Remove 
+    - Search for games in the log
+    
+     Bringing up details on:
+    - All playthroughs
+    - Last Playthrough
+    - Rating
+    - Notes
+*/
+
+fn main() {
+
+    let message_for_lila = "fortnite";
+
+    println!("{}", message_for_lila);
+}
